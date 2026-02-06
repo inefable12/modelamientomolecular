ecab821022711811558788a54c50273ea951e426
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2026-02-06 16:16:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -110,9 +110,11 @@ def page2():
     - Niveles de teoría
     """)
 
-    video1 = "https://youtu.be/5iVqq-indVw" 
-    st.markdown(f'<iframe src="{video1}" width="800" height="450"></iframe>',
-                unsafe_allow_html=True)
+    video_file = open('./imagenes_/video1.mp4','rb')
+    video_bytes = video_file.read()
+    
+    #video1 = "https://youtu.be/5iVqq-indVw" 
+    #st.markdown(f'<iframe src="{video1}" width="800" height="450"></iframe>', unsafe_allow_html=True)
 
 # =========================================================
 # SESIÓN 2
