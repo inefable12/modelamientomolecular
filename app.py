18dc44941338dfe263e35cafd44467388ddf2ca5
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2026-02-07 00:37:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def Home():
                 caption="Representación Molecular. Fuente: Elaboración propia. Generada con la Suite Schrodinger.") 
         
     with col_right:
-        st.markdown("![Alt Text](https://visualizeorgchem.com/images/tutorial-PES/SN2-freq.gif)", caption="Frecuencia imaginaria del TS en SN2. Fuente: Visualize Organic Chemistry (New Jersey Institute of Technology).")
+        st.markdown("![Alt Text](https://visualizeorgchem.com/images/tutorial-PES/SN2-freq.gif)")
         st.image("imagenes_/PES.jpg")
         st.image("imagenes_/tip4p.png", 
                  caption="Modelo de agua para simulaciones de dinámica molecular: TIP4P (arriba) & TIP5P (abajo). Fuente: Elaboración propia. Generada con PyMOL y GROMACS",
